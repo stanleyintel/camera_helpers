be20f7daa71a0bb9775b36e76801bccf1de483ae
Stanley <stanley.chang@intel.com>
2026-06-18 01:23:49 +0800

    update 7_do_post_install.sh for PTL PV2 BKC
    Signed-off-by: Stanley <stanley.chang@intel.com>

--- PTL-PV2_BKC/7_do_post_install.sh
+++ PTL-PV2_BKC/7_do_post_install.sh
@@ -33,7 +33,7 @@ run sudo chmod -R o+rx /etc/camera/
 
 if [ ! -f /etc/udev/rules.d/99-ipu-psys.rules ]; then
   run sudo sh -c 'cat > /etc/udev/rules.d/99-ipu-psys.rules << "EOL"
-KERNEL=="ipu7-psys0", GROUP="video", MODE="0660"
+KERNEL=="ipu7-psys0", ACTION=="add", GROUP="video", MODE="0660"
 EOL'
 fi
 
