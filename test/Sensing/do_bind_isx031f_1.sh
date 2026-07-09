00c0bdbeac96aa70bd7cb20f3e8d862ffa827041
Stanley <stanley.chang@intel.com>
2026-07-09 09:26:15 +0800

    [test] Sensing isx031f
    Signed-off-by: Stanley <stanley.chang@intel.com>

---
+++ test/Sensing/do_bind_isx031f_1.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+set -eux
+
+MEDIA_CTL=${MEDIA_CTL:-/usr/local/bin/media-ctl}
+V4L2_CTL=${V4L2_CTL:-/usr/local/bin/v4l2-ctl}
+MEDIA_DEV=${MEDIA_DEV:-/dev/media0}
+VIDEO_NODE=${VIDEO_NODE:-/dev/video0}
+
+# Active path taken from:
+# 1. config/ipu75xa/sensors/isx031f-1.json
+# 2. media_ctl_p.log
+#
+# Real active chain on this board:
+#   isx031f a-0 -> max9x a-0 -> max9x a -> Intel IPU7 CSI2 0 -> Intel IPU7 ISYS Capture 0
+#
+# The following upstream links are already ENABLED,IMMUTABLE in media_ctl_p.log,
+# so this script does not try to force them:
+#   "isx031f a-0":0 -> "max9x a-0":0
+#   "max9x a-0":2 -> "max9x a":4
+#   "max9x a":0 -> "Intel IPU7 CSI2 0":0
+
+"$MEDIA_CTL" -d "$MEDIA_DEV" -R '"max9x a-0"[0/0->2/0[1]]'
+"$MEDIA_CTL" -d "$MEDIA_DEV" -R '"max9x a"[4/0->0/0[1]]'
+"$MEDIA_CTL" -d "$MEDIA_DEV" -R '"Intel IPU7 CSI2 0"[0/0->1/0[1]]'
+
+"$MEDIA_CTL" -d "$MEDIA_DEV" -V '"isx031f a-0":0/0 [fmt:UYVY8_1X16/1920x1536]'
+"$MEDIA_CTL" -d "$MEDIA_DEV" -V '"max9x a-0":0/0 [fmt:UYVY8_1X16/1920x1536]'
+"$MEDIA_CTL" -d "$MEDIA_DEV" -V '"max9x a-0":2/0 [fmt:UYVY8_1X16/1920x1536]'
+"$MEDIA_CTL" -d "$MEDIA_DEV" -V '"max9x a":4/0 [fmt:UYVY8_1X16/1920x1536]'
+"$MEDIA_CTL" -d "$MEDIA_DEV" -V '"max9x a":0/0 [fmt:UYVY8_1X16/1920x1536]'
+"$MEDIA_CTL" -d "$MEDIA_DEV" -V '"Intel IPU7 CSI2 0":0/0 [fmt:UYVY8_1X16/1920x1536]'
+"$MEDIA_CTL" -d "$MEDIA_DEV" -V '"Intel IPU7 CSI2 0":1/0 [fmt:UYVY8_1X16/1920x1536]'
+
+"$MEDIA_CTL" -d "$MEDIA_DEV" -l '"Intel IPU7 CSI2 0":1 -> "Intel IPU7 ISYS Capture 0":0[1]'
+
+ln -snf "$VIDEO_NODE" /dev/video-isx031f-a-0
+"$V4L2_CTL" -d "$VIDEO_NODE" --set-fmt-video=width=1920,height=1536,pixelformat=UYVY
+
+# Test commands:
+## yavvta
+#  yavta -c10 -f UYVY /dev/video0
+
+## v4l2src
+#  gst-launch-1.0 -e -v v4l2src device=/dev/video0 ! video/x-raw,width=1920,height=1536,format=UYVY,pixel-aspect-ratio=1/1,framerate=30/1 ! videoconvert ! xvimagesink
+
