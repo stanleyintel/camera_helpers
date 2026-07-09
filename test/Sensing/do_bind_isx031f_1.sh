684ef84eb8e43d28dd5fb5eb2ac58ad45f6fddbd
Stanley <stanley.chang@intel.com>
2026-07-09 09:31:51 +0800

    [test] Sensing isx031f (update)
    Signed-off-by: Stanley <stanley.chang@intel.com>

--- test/Sensing/do_bind_isx031f_1.sh
+++ test/Sensing/do_bind_isx031f_1.sh
@@ -2,29 +2,24 @@
 
 set -eux
 
-MEDIA_CTL=${MEDIA_CTL:-/usr/local/bin/media-ctl}
-V4L2_CTL=${V4L2_CTL:-/usr/local/bin/v4l2-ctl}
-MEDIA_DEV=${MEDIA_DEV:-/dev/media0}
-VIDEO_NODE=${VIDEO_NODE:-/dev/video0}
+media-ctl -d /dev/media0 -R '"max9x a-0"[0/0->2/0[1]]'
+media-ctl -d /dev/media0 -R '"max9x a"[4/0->0/0[1]]'
+media-ctl -d /dev/media0 -R '"Intel IPU7 CSI2 0"[0/0->1/0[1]]'
 
-"$MEDIA_CTL" -d "$MEDIA_DEV" -R '"max9x a-0"[0/0->2/0[1]]'
-"$MEDIA_CTL" -d "$MEDIA_DEV" -R '"max9x a"[4/0->0/0[1]]'
-"$MEDIA_CTL" -d "$MEDIA_DEV" -R '"Intel IPU7 CSI2 0"[0/0->1/0[1]]'
+media-ctl -d /dev/media0 -V '"isx031f a-0":0/0 [fmt:UYVY8_1X16/1920x1536]'
+media-ctl -d /dev/media0 -V '"max9x a-0":0/0 [fmt:UYVY8_1X16/1920x1536]'
+media-ctl -d /dev/media0 -V '"max9x a-0":2/0 [fmt:UYVY8_1X16/1920x1536]'
+media-ctl -d /dev/media0 -V '"max9x a":4/0 [fmt:UYVY8_1X16/1920x1536]'
+media-ctl -d /dev/media0 -V '"max9x a":0/0 [fmt:UYVY8_1X16/1920x1536]'
+media-ctl -d /dev/media0 -V '"Intel IPU7 CSI2 0":0/0 [fmt:UYVY8_1X16/1920x1536]'
+media-ctl -d /dev/media0 -V '"Intel IPU7 CSI2 0":1/0 [fmt:UYVY8_1X16/1920x1536]'
 
-"$MEDIA_CTL" -d "$MEDIA_DEV" -V '"isx031f a-0":0/0 [fmt:UYVY8_1X16/1920x1536]'
-"$MEDIA_CTL" -d "$MEDIA_DEV" -V '"max9x a-0":0/0 [fmt:UYVY8_1X16/1920x1536]'
-"$MEDIA_CTL" -d "$MEDIA_DEV" -V '"max9x a-0":2/0 [fmt:UYVY8_1X16/1920x1536]'
-"$MEDIA_CTL" -d "$MEDIA_DEV" -V '"max9x a":4/0 [fmt:UYVY8_1X16/1920x1536]'
-"$MEDIA_CTL" -d "$MEDIA_DEV" -V '"max9x a":0/0 [fmt:UYVY8_1X16/1920x1536]'
-"$MEDIA_CTL" -d "$MEDIA_DEV" -V '"Intel IPU7 CSI2 0":0/0 [fmt:UYVY8_1X16/1920x1536]'
-"$MEDIA_CTL" -d "$MEDIA_DEV" -V '"Intel IPU7 CSI2 0":1/0 [fmt:UYVY8_1X16/1920x1536]'
+media-ctl -d /dev/media0 -l '"Intel IPU7 CSI2 0":1 -> "Intel IPU7 ISYS Capture 0":0[1]'
 
-"$MEDIA_CTL" -d "$MEDIA_DEV" -l '"Intel IPU7 CSI2 0":1 -> "Intel IPU7 ISYS Capture 0":0[1]'
-
-"$V4L2_CTL" -d "$VIDEO_NODE" --set-fmt-video=width=1920,height=1536,pixelformat=UYVY
+v4l2-ctl -d /dev/video0 --set-fmt-video=width=1920,height=1536,pixelformat=UYVY
 
 # Test commands:
-## yavvta
+## yavta
 #  yavta -c10 -f UYVY /dev/video0
 
 ## v4l2src
