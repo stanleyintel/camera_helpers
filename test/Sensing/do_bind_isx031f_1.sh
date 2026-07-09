4fc83dc39563d15b18d4a678ff3faee8cc661e06
Stanley <stanley.chang@intel.com>
2026-07-09 09:27:57 +0800

    [test] Sensing isx031f (update)
    Signed-off-by: Stanley <stanley.chang@intel.com>

--- test/Sensing/do_bind_isx031f_1.sh
+++ test/Sensing/do_bind_isx031f_1.sh
@@ -7,19 +7,6 @@ V4L2_CTL=${V4L2_CTL:-/usr/local/bin/v4l2-ctl}
 MEDIA_DEV=${MEDIA_DEV:-/dev/media0}
 VIDEO_NODE=${VIDEO_NODE:-/dev/video0}
 
-# Active path taken from:
-# 1. config/ipu75xa/sensors/isx031f-1.json
-# 2. media_ctl_p.log
-#
-# Real active chain on this board:
-#   isx031f a-0 -> max9x a-0 -> max9x a -> Intel IPU7 CSI2 0 -> Intel IPU7 ISYS Capture 0
-#
-# The following upstream links are already ENABLED,IMMUTABLE in media_ctl_p.log,
-# so this script does not try to force them:
-#   "isx031f a-0":0 -> "max9x a-0":0
-#   "max9x a-0":2 -> "max9x a":4
-#   "max9x a":0 -> "Intel IPU7 CSI2 0":0
-
 "$MEDIA_CTL" -d "$MEDIA_DEV" -R '"max9x a-0"[0/0->2/0[1]]'
 "$MEDIA_CTL" -d "$MEDIA_DEV" -R '"max9x a"[4/0->0/0[1]]'
 "$MEDIA_CTL" -d "$MEDIA_DEV" -R '"Intel IPU7 CSI2 0"[0/0->1/0[1]]'
@@ -34,7 +21,6 @@ VIDEO_NODE=${VIDEO_NODE:-/dev/video0}
 
 "$MEDIA_CTL" -d "$MEDIA_DEV" -l '"Intel IPU7 CSI2 0":1 -> "Intel IPU7 ISYS Capture 0":0[1]'
 
-ln -snf "$VIDEO_NODE" /dev/video-isx031f-a-0
 "$V4L2_CTL" -d "$VIDEO_NODE" --set-fmt-video=width=1920,height=1536,pixelformat=UYVY
 
 # Test commands:
