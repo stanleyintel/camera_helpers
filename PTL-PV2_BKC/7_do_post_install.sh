59dbde1065f95d728ae1d6d7a932ce5a0854ff4f
Stanley <stanley.chang@intel.com>
2026-08-07 12:31:29 +0800

    [ptl pv2 bkc] update for adding packges
    Signed-off-by: Stanley <stanley.chang@intel.com>

--- PTL-PV2_BKC/7_do_post_install.sh
+++ PTL-PV2_BKC/7_do_post_install.sh
@@ -24,7 +24,9 @@ run() {
 
 run /bin/rm -rf $HOME/.cache/gstreamer-1.0
 
-run sudo -E apt install -y v4l-utils
+# dkms is to build dkms drivers
+# meson is to build v4l-utils
+run sudo -E apt install -y dkms yavta meson
 
 run sudo chmod o+rx /usr/lib/libcamhal
 run sudo chmod o+rx /usr/lib/libcamhal/plugins/
